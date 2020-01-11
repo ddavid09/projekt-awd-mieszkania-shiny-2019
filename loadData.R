17763d10616c17813766589f54aa28cf042e6d24
ddavid09 <ddawidziak.dd@gmail.com>
2020-01-11 18:11:29 +0100

    Delete BydWaw loadData code

--- loadData.R
+++ loadData.R
@@ -23,25 +23,4 @@ daneWoj<-daneWoj %>% rename(
 )
 write.xlsx(daneWoj, "mieszkania-woj.xlsx")
 #
-liczSprzedanychBydWaw<-read.csv2("01-liczba-sprzedanych-mieszkan-byd-waw.csv", encoding = "UTF-8")
-medianaCen1m2BydWaw<-read.csv2("01-mediana-cen-za-1m2-byd-waw.csv", encoding = "UTF-8")
-sredniaCenaMBydWaw<-read.csv2("01-srednia-cena-mieszkan-byd-waw.csv", encoding = "UTF-8")
-sredniaCena1m2BydWaw<-read.csv2("01-srednia-cena-za-1m2-byd-waw.csv", encoding = "UTF-8")
-daneBydWaw<-liczSprzedanychBydWaw
-daneBydWaw$Jednostka.miary<-NULL
-daneBydWaw$Atrybut<-NULL
-daneBydWaw$Kod<-NULL
-daneBydWaw$X<-NULL
-medianaCen1m2BydWaw<-medianaCen1m2BydWaw %>% pull(Wartosc)
-sredniaCenaMBydWaw<-sredniaCenaMBydWaw %>% pull(Wartosc)
-sredniaCena1m2BydWaw<-sredniaCena1m2BydWaw %>% pull(Wartosc)
-daneBydWaw$Mediana.m2<-medianaCen1m2BydWaw
-daneBydWaw$Srednia.Cena.M<-sredniaCenaMBydWaw
-daneBydWaw$Srednia.Cena.m2<-sredniaCena1m2BydWaw
-daneBydWaw<-daneBydWaw %>% rename(
-  Powiat=Nazwa,
-  Rynek=Transakcje.rynkowe,
-  Metraz=Powierzchnia.uzytkowa.lokali.mieszkalnych,
-  Liczba.Sprzedanych.M=Wartosc
-)
-write.xlsx(daneBydWaw,"mieszkania-bydwaw.xlsx")
+
