7aa80e1b24766faea86e93f13094583461d114eb
ddavid09 <ddawidziak.dd@gmail.com>
2020-01-11 09:40:51 +0100

    3 plots working, bigger resolution, sizes and fonts

--- app.R
+++ app.R
@@ -17,11 +17,12 @@ ui <- fluidPage(
                                 selectInput("modeWoj",
                                             choices=list(
                                                 "średnie ceny za m2" = 1,
-                                                "liczba sprzedanych mieszkań" = 2
+                                                "liczba sprzedanych mieszkań(rok/woj)" = 2,
+                                                "liczba sprzedanych mieszkań(cena)" = 3
                                             ),
                                             label="Wybierz dane do Analizy",
                                             selected=1
-                                    
+                                            
                                 ),
                                 
                                 
@@ -33,8 +34,8 @@ ui <- fluidPage(
                                 ),
                                 
                                 selectInput("RynekInput",
-                                             label="Wybierz rynek",
-                                             choices=factor(daneWoj$Rynek)%>%levels()
+                                            label="Wybierz rynek",
+                                            choices=factor(daneWoj$Rynek)%>%levels()
                                 ),
                                 conditionalPanel(
                                     condition="input.modeWoj == 1",
@@ -47,9 +48,9 @@ ui <- fluidPage(
                                 conditionalPanel(
                                     condition="input.modeWoj == 2",
                                     selectInput("RokSelectInput",
-                                                       label="Wybierz rok",
-                                                       choices=years,
-                                                       selected=years[1]
+                                                label="Wybierz rok",
+                                                choices=years,
+                                                selected=years[1]
                                     ),
                                     checkboxGroupInput("MetrazeGroup",
                                                        label="Wybierz metraż",
@@ -62,10 +63,21 @@ ui <- fluidPage(
                                                        ),
                                                        selected="do 40 m2"
                                     )
-                                )
+                                ),
+                                conditionalPanel(
+                                    condition="input.modeWoj == 3",
+                                    selectInput("RokSelectInput3",
+                                                label="Wybierz rok",
+                                                choices=years,
+                                                selected=years[1]
+                                    ),
+                                ),
+                                width = 4
                             ),
                             mainPanel(
-                                plotOutput("plotWoj1")
+                                plotOutput(outputId = "plotWoj1",
+                                           width = "100%",
+                                           height = "100%")
                             )
                         )   
                ),
@@ -97,27 +109,44 @@ server <- function(input, output) {
             ymax<-round(max(as.vector(plotData[,9])),digits=-4)
             ystep<-ymax/10
             
-            ggplot(plotData, aes(x=Wojewodztwo, y=Srednia.Cena.m2, fill=factor(Rok))) +
+            gout <- ggplot(plotData, aes(x=Wojewodztwo, y=Srednia.Cena.m2, fill=factor(Rok))) +
                 geom_bar(stat="identity", position=position_dodge()) + theme(axis.text.x = element_text(angle = 65, hjust = 1)) +
                 labs(title="Średnie ceny w wojewodztwach", fill="Rok") + 
                 scale_y_continuous(limits=c(0, ymax)) +
                 geom_point(aes(y=Mediana.m2), position=position_dodge(width=0.9), shape=4) +
                 ylab("Średnia cena za 1 m2")
             
-        }else{
+        }else if(input$modeWoj == 2){
             plotDataWoj<-filter(daneWoj, Wojewodztwo %in% input$WojewodztwaCheckGroup)
             plotDataMarket<-filter(plotDataWoj, Rynek==input$RynekInput)
             plotDataArea<-filter(plotDataMarket, Metraz %in% input$MetrazeGroup)
             plotDataYear<-filter(plotDataArea, Rok == input$RokSelectInput)
             plotData<-plotDataYear
             
-            ggplot(plotData, aes(x=Wojewodztwo, y=Liczba.Sprzedanych.M, fill=factor(Metraz))) +
+            gout <- ggplot(plotData, aes(x=Wojewodztwo, y=Liczba.Sprzedanych.M, fill=factor(Metraz))) +
                 geom_bar(stat="identity", position=position_dodge()) + theme(axis.text.x = element_text(angle = 65, hjust = 1)) +
                 labs(title="Liczba sprzedanych mieszkan", fill="Metraz") + 
                 ylab("Liczba sprzedanych mieszkan")
+        }else{
+            plotDataWoj<-filter(daneWoj, Wojewodztwo %in% input$WojewodztwaCheckGroup)
+            plotDataMarket<-filter(plotDataWoj, Rynek==input$RynekInput)
+            plotDataArea<-filter(plotDataMarket, Metraz %!in% c("do 40 m2","od 40,1 do 60 m2", "od 60,1 do 80 m2","od 80,1 m2"))
+            plotDataYear<-filter(plotDataArea, Rok %in% input$RokSelectInput3)
+            plotData<-plotDataYear
+            ymax<-round(max(as.vector(plotData[,6])))
+            ystep<-ymax/10
+            
+            gout <- ggplot(plotData, aes(x=Srednia.Cena.m2, y=Liczba.Sprzedanych.M, color=Wojewodztwo, label=Wojewodztwo)) +
+                geom_point(show.legend = F, size=10) + geom_text(show.legend = F, angle=45, hjust=0.5, vjust=2, size=7) +
+                labs(title="Liczba sprzedanych mieszkań w zależności od ceny za 1 m2") +
+                xlab("Średnia cena za 1 m2") + ylab("Liczba sprzedanych mieszkan") + 
+                scale_y_continuous(limits=c(0, ymax))
         }
-       
-    })
+        gout + theme(axis.text=element_text(size=14),
+                  axis.title=element_text(size=16,face="bold"),
+                  plot.title = element_text(size=28, face="bold"))
+        
+    }, width = 1200, height = 800)
 }
 
 
